37e2241e4b711adeb8afdc97b57c035ddc8b5d9a
Timo Hohmann <kontakt@timo-hohmann.de>
2025-07-19 11:26:57 +0200

    [Enhancement] Optimize Dockerfiles and prod build and fix ALLOWED_DEV_ORIGIN var in example .env

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,20 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  ...(isProd && { output: 'standalone' }),
+
   typescript: {
     ignoreBuildErrors: true,
   },
-  
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-  
+
   images: {
     remotePatterns: [
       {
